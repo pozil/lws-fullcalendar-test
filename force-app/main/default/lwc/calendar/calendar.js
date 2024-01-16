22da1878a6402284427e22e9bf1ade99d1a2a66e
Philippe Ozil <pozil@salesforce.com>
2024-01-16 22:33:48 +0100

    feat: async/await

--- force-app/main/default/lwc/calendar/calendar.js
+++ force-app/main/default/lwc/calendar/calendar.js
@@ -4,39 +4,41 @@ import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import FULL_CALENDAR from '@salesforce/resourceUrl/fullCalendar';
 
 export default class Calendar extends LightningElement {
-  isCalInitialized = false;
+    isCalInitialized = false;
 
-  renderedCallback() {
-    if (this.isCalInitialized) {
-      return;
+    async renderedCallback() {
+        if (this.isCalInitialized) {
+            return;
+        }
+        this.isCalInitialized = true;
+
+        try {
+            await Promise.all([
+                loadScript(this, FULL_CALENDAR + '/main.min.js'),
+                loadStyle(this, FULL_CALENDAR + '/main.min.css')
+            ]);
+            this.initializeCalendar();
+        } catch (error) {
+            console.error('Failed to load FullCalendar lib: ', error);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error loading calendar',
+                    message: error.message,
+                    variant: 'error'
+                })
+            );
+        }
     }
-    this.isCalInitialized = true;
 
-    Promise.all([
-      loadScript(this, FULL_CALENDAR + '/main.min.js'),
-      loadStyle(this, FULL_CALENDAR + '/main.min.css')
-    ])
-      .then(() => {
-        this.initializeCalendar();
-      })
-      .catch((error) => {
-        console.error(JSON.stringify(error));
-        this.dispatchEvent(
-          new ShowToastEvent({
-            title: 'Error loading calendar',
-            message: error.message,
-            variant: 'error'
-          })
-        );
-      });
-  }
-
-  initializeCalendar() {
-    const calendarEl = this.template.querySelector('.calendar');
-    // eslint-disable-next-line no-undef
-    const calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
-    });
-    calendar.render();
-  }
+    initializeCalendar() {
+        const calendarEl = this.template.querySelector('.calendar');
+        if (!window.FullCalendar) {
+            throw new Error('FullCalendar is not loaded in window contex.');
+        }
+        // eslint-disable-next-line no-undef
+        const calendar = new window.FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth'
+        });
+        calendar.render();
+    }
 }
